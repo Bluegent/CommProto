e058fd30bb478c49decc3c0d1cd4b96bec18a3d3
Bluegent <bluegentleman@live.com>
2021-05-02 19:27:54 +0300

    Added some more string utils

--- CommProtoLib/interface/commproto/utils/String.h
+++ CommProtoLib/interface/commproto/utils/String.h
@@ -8,6 +8,11 @@ namespace commproto
 	namespace utils
 	{
 		void replaceAll(std::string& target, const std::string& substr, const std::string& replace);
+
+
+		std::string getString(const uint32_t number);
+
+		std::string getString(const float value, const uint32_t precision = 3);
 	}
 }
 
--- CommProtoLib/src/utils/String.cpp
+++ CommProtoLib/src/utils/String.cpp
@@ -1,4 +1,5 @@
 #include <commproto/utils/String.h>
+#include <sstream>
 
 namespace commproto
 {
@@ -12,6 +13,21 @@ namespace commproto
 				target.replace(it, substr.size(), replace);
 				it = target.find(substr, it + replace.size());
 			}
+		}
+
+		std::string getString(const uint32_t number)
+		{
+			std::stringstream stream;
+			stream << number;
+			return stream.str();
+		}
+
+		std::string getString(const float value, const uint32_t precision)
+		{
+			std::stringstream stream;
+			stream.precision(precision);
+			stream << value;
+			return stream.str();
 		}
 	}
 }
